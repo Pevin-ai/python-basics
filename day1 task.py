3a9312970afe54757fb667d7a002613fe234d469
PEVIN S <pevinaids@gmail.com>
2026-02-20 18:51:26 +0530

    variables and data types

---
+++ day1 task.py
@@ -0,0 +1,5 @@
+subject="maths"
+CGPA=8.76
+datascience=False
+print(f"i love {subject}.my CGPA is{CGPA}.learning data science:{datascience}.")
+
---
+++ day1 task2.py
@@ -0,0 +1,7 @@
+name="pevin"
+age=18
+CGPA=8.76
+AI=True
+skills=["python","c+","java"]
+print(f"my name is {name},i am {age} years old,my CGPA is {CGPA},interested in AI:{AI},my skills are{skills}.")
+
