708187d62b0a7afdda6ffe1b30a3e6c54c22ff0e
PEVIN S <pevinaids@gmail.com>
2026-02-21 18:16:26 +0530

    DAY 2

---
+++ day2 task2.py
@@ -0,0 +1,17 @@
+name=input("enter your name: ")
+sub1=int(input("enter your maths mark: "))
+sub2=int(input("enter your science mark: "))
+sub3=int(input("enter your english marks: "))
+total=sub1+sub2+sub3
+avg=total/3
+percentage=(total/300)*100
+print("Hello",name)
+print("Total=",total)
+print("AVg=",avg)
+print("Percentage=",percentage)
+
+
+
+
+
+
