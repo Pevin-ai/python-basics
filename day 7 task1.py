fc8752a2b552f9f2d21432dab26658f87ea59dab
PEVIN S <pevinaids@gmail.com>
2026-02-26 18:19:02 +0530

    list

---
+++ day 7 task1.py
@@ -0,0 +1,9 @@
+number=[]
+for i in range(5):
+    list=int(input("enter the number:"))
+    number.append(list)
+print("list",number)
+print("total sum",sum(number))
+print("maximum",max(number))
+print("minimum",min(number))
+
